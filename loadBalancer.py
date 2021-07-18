82fd4fd80a32022ec87a4cf707ae7422406a9736
taratt <taratt.saba@gmail.com>
2021-07-19 03:29:34 +0430

    job done!!! we're dying

--- loadBalancer.py
+++ loadBalancer.py
@@ -6,7 +6,7 @@ import os
 
 requestsQueue = []
 last_id = int(time.time()) % 100000
-
+containerDic = {}
 def parse(request):
     global requestsQueue
     global last_id
@@ -27,36 +27,52 @@ def parse(request):
     requestsQueue.append(tmp)
     return tmp
 
-def balancer():
-    #write your code from here
-    # tasks in requestsQueue
-    # format of each task: [id,req1, req2, ..., reqn, outputdirectory]
-    # format of each request: [method, inputFile]
-    
-    
-    client = docker.from_env()
-    #container = client.containers.run('loadbalancer/test:v1', detach=True, )
-    #print(container.id)
-    print(client.images.list())
-    id= "729287d05935"
-    container = client.containers.get(id)
 
+def makeContainers(client):
+
+    for i in range(3):
+        container = client.containers.run("loadbalancer/losers:version1.0", detach=True, tty=True)
+        containerDic[container.id] = True
+
+    return containerDic
+
+
+def balancer():
+    client = docker.from_env()
+    makeContainers(client)
+    #print(container.id)
+    # print(client.images.list())
+    # id= "729287d05935"
+    # container = client.containers.get(id)
+    # id = containers[0]
+    # container = client.containers.get(id)
     while True:
         if len(requestsQueue) > 0:
+            idle = None
 
             curr_request = requestsQueue.pop(0)
             curr_reqid = curr_request.pop(0)
             output_dir = curr_request.pop(-1)
-            
+
             print(curr_reqid, output_dir)
             for command in curr_request:
+                while not idle:
+                    for container in containerDic.keys():
+                        if containerDic[container] == True:
+                            idle = container
+                            break
                 cmd_name ,inputfile_path = command
-
-                copy_file(inputfile_path, id)
-
+                # print("###############")
+                copy_file(inputfile_path, str(idle))
+                # print("iiiiiiiiii###############")
                 file_name = os.path.basename(inputfile_path)
-                exec_command(container, curr_reqid, cmd_name, file_name, output_dir)
-    
+                #make thread
+                container = client.containers.get(idle)
+                containerThread = threading.Thread(target=exec_command, args=(container,curr_reqid,cmd_name, file_name,output_dir,))
+                containerThread.daemon = True
+                containerThread.start()
+            #print(client)
+
 def copy_file(inputfile_path, container_id):
     copy_file_command = "docker cp " + inputfile_path + " " + container_id + ":/home/loadBalancer/in"
     print("copy command:", copy_file_command)
@@ -66,21 +82,23 @@ def copy_file(inputfile_path, container_id):
 def exec_command(container, reqid, cmd_name, inputfile_name, output_dir):
     exec_command_str = "python fileProcessor.py " + cmd_name + " " + "./in/" + inputfile_name
     print("exec string:", exec_command_str)
-    
-    #output: a tuple of (exit_code, output)
+    containerDic[container.id] =False
+    print("heyyyy",container.id)
     status_code, output_bytes = container.exec_run(exec_command_str, stdout=True)
     print(status_code, output_bytes)
 
-    if status_code == 0: 
+    if status_code == 0:
         outputfile_name = cmd_name + str(reqid) + ".txt"
         outputfile = open(output_dir + "/" + outputfile_name, "w")
 
         outputfile.write(output_bytes.decode("utf-8"))
         outputfile.close()
 
+    containerDic[container.id] = True
+
+
 
 def main():
-    
     loadbalancerThread = threading.Thread(target=balancer)
     loadbalancerThread.daemon = True
     loadbalancerThread.start()
