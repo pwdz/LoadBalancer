74977a262efd431e73199c29d434066cd5a76bf2
Mohammad ebrahim Adibzadeh <me.adibzadeh@gmail.com>
2021-07-23 14:31:41 +0430

    Add sampleInputs

---
+++ sampleInput/sampleCpp.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	cout<<"let's gooo"<<endl;
+	for(int i=0;i<10;i++){
+		cout<<i*3<<endl;
+	}
+
+	return 0;
+}
---
+++ sampleInput/samplePy.py
@@ -0,0 +1,3 @@
+print("python is python!")
+for i in range(10):
+    print(i * 7)
